c3a9ae318059257bb9e20f9b6c57420862bb9aa6
Tejaswi Tipparti <tejaswi@neetable.com>
2018-11-30 09:43:47 +0530

    Cleanup

--- server.js
+++ server.js
@@ -3,86 +3,63 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var fs = require('fs');
-var cors = require('cors');
+const cors = require('cors');
 var fm = require('front-matter');
 var marked = require('marked');
-var router = express.Router();   
+var router = express.Router();
 
 app.use(cors());
 
 app.use(express.static('public'))
 
-// var fullPath = [],  blog = null;
+let blogFolderPath = [], blog = null;
+let blogList = [];
 
-// var logFinal = [];
-// var showData  = null;
-// var cssFileCount = 0;
+traverseDir('ClientApp/src/blogs');
 
-// var glob = require( 'glob' );  
-
-// glob( 'ClientApp/src/blogs/*.md', function( err, files ) {
-//    return cssFileCount = (files.length);
-// });
-
-
-// traverseDir('ClientApp/src/blogs');
-//  function traverseDir(dir) {
-//    fs.readdirSync(dir).forEach(file => {
-//      fullPath = path.join(dir, file);
-//           if (fs.lstatSync(fullPath).isDirectory()) {
-//             traverseDir(fullPath);
-//             readFile(fullPath)
-//           } else {
-//             readFile(fullPath)
-//           }
-//    });
-// }
-
-// function readFile() {
-//   var md = require("markdown").markdown;
-//       fs.readFile(fullPath, {encoding: 'utf-8'}, function(err,data){
-//         if (!err) {
-//             blog= fm(data);
-//             showData = (JSON.stringify(blog) + ',');
-//             setApi(showData);
-//         } else {
-//             console.log(err);
-//         }
-//     });
-// }
-
-// function setApi(data){
-//   var dataTest = data.substring(0, data.length - 1);
-//   logFinal.push(JSON.parse(dataTest));
-
-//   app.get('/api/listBlogs', function (req, res) {
-     
-//     }) res.send(logFinal)
-// }
-app.use(function(req, res) {
-     res.send('404: Page not Found', 404);
-  });
-  
-  // Handle 500
-  app.use(function(error, req, res, next) {
-     res.send('500: Internal Server Error', 500);
-  });
-
-if(process.env.NODE_ENV === 'production') {
-	app.use(express.static('public/build'));
-  app.get("*", function(req, res) {
-    res.sendFile(path.resolve(__dirname, 'public', 'build', 'index.html')); 
-});
-  app.use(function(req, res) {
-     res.send('404: Page not Found', 404);
-  });
-  
-  // Handle 500
-  app.use(function(error, req, res, next) {
-     res.send('500: Internal Server Error', 500);
+function traverseDir(dir) {
+  fs.readdirSync(dir).forEach(file => {
+    blogFolderPath = path.join(dir, file);
+    if (fs.lstatSync(blogFolderPath).isDirectory()) {
+      traverseDir(blogFolderPath);
+      readFile(blogFolderPath)
+    } else {
+      readFile(blogFolderPath)
+    }
   });
 }
 
-app.listen(1113, function(){
+function readFile() {
+  var md = require("markdown").markdown;
+  fs.readFile(blogFolderPath, { encoding: 'utf-8' }, function (err, data) {
+    if (!err) {
+      blog = fm(data);
+      let showData = (JSON.stringify(blog) + ',');
+      setApi(showData);
+    } else {
+      console.log(err);
+    }
+  });
+}
+
+function setApi(data) {
+  let dataTest = data.substring(0, data.length - 1);
+  blogList.push(JSON.parse(dataTest));
+
+  app.get('/api/blogs', function (req, res) {
+    res.send(blogList)
+  })
+}
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('public/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'build', 'index.html'));
+  })
+}
+
+app.listen(1113, function () {
   console.log('app is listening on port 1113');
 })
